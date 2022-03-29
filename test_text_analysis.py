de72dc5bf0135781ef8ba440b1edcc6e9a5aab29
Malvina Clavering <malvina.clavering@gmail.com>
2022-03-29 17:14:19 -0400

    Add more unit tests, create funtion file

--- test_text_analysis.py
+++ test_text_analysis.py
@@ -1,16 +1,20 @@
-from gettext import find
+
 import nltk
 import os
 
-sample_text = "It was a tall tree. The squirrel lost its tiny hat. It had to \
+sample_text1 = "It was a tall tree. The squirrel lost its tiny hat. It had to \
     climb down the tree to find the hat. 'That's unfortunate!'"
-sample_tokens = nltk.word_tokenize(sample_text)
+sample_tokens1 = nltk.word_tokenize(sample_text1)
+sample_text2 = "Miriam bought a deliciously moist chapstick from Trader \
+    Johan's. Malvina's mother's crispy crackers make a distracted \
+    Miriam very happy."
+sample_tokens2 = nltk.word_tokenize(sample_text2)
 
 def test_find_sentences_with_keyword():
     """
-    Test that the function corectly finds all sentences containing the keyword
+    Test that the function correctly finds all sentences containing the keyword
     """
-    assert find_sentences_with_keyword("hat", sample_tokens) == ["The squirrel \
+    assert find_sentences_with_keyword("hat", sample_tokens1) == ["The squirrel \
         lost its tiny hat.","It had to climb down the tree to find the hat."]
 
 def test_find_occurances_of_keyword():
@@ -19,24 +23,53 @@ def test_find_occurances_of_keyword():
     keyword exactly
     """
     # Test a single word that occurs once in a string
-    assert find_occurances_of_keyword("squirrel", sample_tokens) == [7]
+    assert find_occurances_of_keyword("squirrel", sample_tokens1) == [7]
     # Test that a lowercase keyword is found only in lowercase form
-    assert find_occurances_of_keyword("the", sample_tokens) == [18, 22]
+    assert find_occurances_of_keyword("the", sample_tokens1) == [18, 22]
     # Test that words inside of other words are not counted as occurances
-    assert find_occurances_of_keyword("hat", sample_tokens) == [11, 23]
-    # Test that searching for a word not in the string returns
+    assert find_occurances_of_keyword("hat", sample_tokens1) == [11, 23]
+    # Test that searching for a word not in the string returns an empty string
+    assert find_occurances_of_keyword("bread", sample_tokens1) == []
 
 def test_find_adjectives():
-    pass
-
-def test_find_words_that_describe_keyword():
-    pass
+    """
+    Check that the function correctly identifies the indexes all adjectives in
+    the string that occur befor the keyword
+    """
+    parts_of_speech1 = nltk.pos_tag(sample_tokens1)
+    parts_of_speech2 = nltk.pos_tag(sample_tokens2)
+    # Test find adjectives for a single word
+    assert find_adjectives([4], parts_of_speech) == ["tall"]
+    # Test find adjectives for multiple words
+    assert find_adjectives([4, 11], parts_of_speech) == ["tall", "tiny"]
+    # Test find adjectives in a string with multiple other adjectives.
+    assert find_adjectives([18], parts_of_speech2) == ["crispy"]
+    # Test that a word that is not a noun returns no adjectives.
+    assert find_adjectives([1], parts_of_speech2) == []
 
 def test_look_for_adjectives():
-    pass
+    """
+    Test the spacy implementation of adjective finder
+    """
+    # Test find adjectives for a single word
+    assert find_adjectives("tree", sample_text1[0:12]) == ["tall"]
+    # Test find adjectives in a string with multiple other adjectives.
+    assert find_adjectives("crackers", sample_text2[12:26]) == ["crispy"]
+    # Test that a word that is not a noun returns no adjectives.
+    assert find_adjectives("It", sample_text1[6:12]) == []
+    assert find_adjectives("moist", sample_text2[0:11]) == []
+    # Check that an adjective separated by one word from keyword is found.
+    assert find_adjectives("Miriam", sample_text2[12:27]) == ["happy"]
 
-def test_find_adj_in_all_sentences():
-    pass
 
 def test_expand_keywords():
+    """ 
+    Check that the function returns the regular plural and capitalized version\
+    of a word
+    """
+    # Check that a sinngle keyword is expanded.
+    assert expand_keywords(["bath"]) == ["bath", "Bath", "baths", "Baths"]
+    # Check that multiple keywords are expanded.
+    assert expand_keywords(["house", "frog"]) == ["house", "frog", "House",\
+         "houses", "Houses", "Frog", "frogs", "Frogs"]
     pass
---
+++ text_analysis.py
@@ -0,0 +1,115 @@
+import nltk
+import os
+import spacy
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+from nltk.sentiment import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+
+
+from WordBank import word_bank
+
+def find_occurances_of_keyword(keyword,text):
+    list_of_indexes=[]
+    for index,word in enumerate(text):
+        if keyword == word:
+            list_of_indexes.append(index)
+    return list_of_indexes
+
+def find_sentences_with_keyword(keyword, text):
+    """
+    Find the sentence that contextualizes the occurence of a keyword and return a list
+    of indices that represent the range of each sentence containing the keyword.
+    
+    Args:
+        keyword: a string representing a word to contextualize in a sentence.
+        text: a tokenized text given as a list of strings of words and punctuation.
+        
+    Returns:
+        A list of strings that are sentences containing the keyword.
+    """
+    keyword_locations = find_occurances_of_keyword(keyword, text)
+    end_punctuation = [".", "!", "?"]
+    sentence_ranges = []
+    for i in keyword_locations:
+        next_word = i + 1
+        while text[next_word] not in end_punctuation:
+            next_word += 1
+        end_location = next_word + 1
+        previous_word = i-1
+        while text[previous_word] not in end_punctuation:
+            previous_word -= 1
+        start_location = previous_word + 1
+        if (start_location, end_location) not in sentence_ranges:
+            sentence_ranges.append((start_location, end_location))
+    sentences=[]
+    for range_ in sentence_ranges:
+        new_sentence=TreebankWordDetokenizer().detokenize((text[range_[0]:range_[1]]))
+        sentences.append(new_sentence.replace(" ’ ", "’"))
+    return sentences
+
+def look_for_adjectives(word,sentence):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(sentence)
+    adjectives=[]
+    for token in doc:
+        #print(token.text +" "+token.dep_+" "+token.head.text+" "+token.pos_)
+        working_token=token
+        while working_token.dep_ != "ROOT":
+            if working_token.head.text==word and (token.pos_==("ADJ")):
+                adjectives.append(token.text)
+                break
+            if working_token.head.text==working_token.text:
+                break
+            working_token=working_token.head
+    return adjectives
+
+def find_adj_in_all_sentences(keyword,path):
+    with open(path) as book:
+        contents = book.read()
+        text = nltk.word_tokenize(contents)
+        #parts_of_speech = nltk.pos_tag(tokens)
+    adj=[]
+    for item in find_sentences_with_keyword(keyword, text):
+        adj+=look_for_adjectives(keyword,item)
+    return adj
+
+def expand_keywords(keywords_):
+    new_keywords=keywords_[:]
+    for word in keywords_:
+        new_keywords.append(word+"s")
+        new_keywords.append(word.capitalize())
+        new_keywords.append((word+"s").capitalize())
+    return new_keywords
+
+def find_adj_in_all_books(keywords):
+    keywords=expand_keywords(keywords)
+    adj=[]
+    for word in keywords:
+        for book in os.listdir("BooksCleaned"):
+            adj+=(find_adj_in_all_sentences(word,f'BooksCleaned/{book}')) 
+        return(adj)
+        
+def find_adj_all_words_all_books(wordbank):
+    adj_list = []
+    for list in wordbank:
+        adj_list.append(find_adj_in_all_books(list))
+    return adj_list
+
+
+def sentiment_countifier_individual(adjectives):
+    sia = SentimentIntensityAnalyzer()
+    pos_scores=[]    
+    neu_scores=[]    
+    neg_scores=[] 
+    for word in adjectives:    
+        sentiments = sia.polarity_scores(word)
+        print(sentiments)
+        pos_scores.append(sentiments["pos"])
+        neu_scores.append(sentiments["neu"])
+        neg_scores.append(sentiments["neg"])
+    pos_score= sum(pos_scores) / len(pos_scores)
+    neu_score= sum(neu_scores) / len(neu_scores)
+    neg_score= sum(neg_scores) / len(neg_scores)    
+    return [pos_score,neu_score,neg_score]
