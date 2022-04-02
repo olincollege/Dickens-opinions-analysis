34b25caae3c69f50ac2a7627257f5f03d7f4f691
Malvina Clavering <malvina.clavering@gmail.com>
2022-04-01 20:33:56 -0400

    Finalize tests

--- test_text_analysis.py
+++ test_text_analysis.py
@@ -14,6 +14,7 @@ sample_tokens2 = nltk.word_tokenize(sample_text2)
 sample_text3 = "She had sharp and hairy elbows."
 sample_tokens3 = nltk.word_tokenize(sample_text3)
 
+
 def test_find_occurances_of_keyword():
     """
     Test that the function finds the indexes of all strings that match the
@@ -28,46 +29,42 @@ def test_find_occurances_of_keyword():
     # Test that searching for a word not in the string returns an empty string
     assert find_occurances_of_keyword("bread", sample_tokens1) == []
 
+
 def test_find_sentences_with_keyword():
     """
     Test that the function correctly finds all sentences containing the keyword
     """
-    assert find_sentences_with_keyword("hat", sample_tokens1) == ["The squirrel \
-        lost its tiny hat.","It had to climb down the tree to find the hat."]
-    assert find_sentences_with_keyword("crackers", sample_tokens2) == ["Malvina's mother's crispy crackers make a distracted \
-    Miriam very happy."]
+    # Check for a single sentence string
+    assert find_sentences_with_keyword("sharp", sample_tokens3) == [
+        "She had sharp and hairy elbows."]
+    # Check for a a multiple sentence string
+    assert find_sentences_with_keyword("crackers", sample_tokens2) == \
+        ["Malvina's mother's crispy crackers make a distracted Miriam very "
+        + "happy."]
+    # Check for a a multiple sentence string, word in multiple sentences
+    assert find_sentences_with_keyword("Miriam", sample_tokens2) == [
+        "Miriam bought a deliciously moist chapstick from Trader Johan's.",
+        "Malvina's mother's crispy crackers make a distracted Miriam very"
+        + " happy."]
 
-# Obsolete
-def test_find_adjectives():
-    """
-    Check that the function correctly identifies the indexes all adjectives in
-    the string that occur befor the keyword
-    """
-    parts_of_speech1 = nltk.pos_tag(sample_tokens1)
-    parts_of_speech2 = nltk.pos_tag(sample_tokens2)
-    # Test find adjectives for a single word
-    assert find_adjectives([4], parts_of_speech1) == ["tall"]
-    # Test find adjectives for multiple words
-    assert find_adjectives([4, 11], parts_of_speech1) == ["tall", "tiny"]
-    # Test find adjectives in a string with multiple other adjectives.
-    assert find_adjectives([18], parts_of_speech2) == ["crispy"]
-    # Test that a word that is not a noun returns no adjectives.
-    assert find_adjectives([1], parts_of_speech2) == []
 
 def test_look_for_adjectives():
     """
     Test the spacy implementation of adjective finder
     """
     # Test find adjectives for a single word
-    assert find_adjectives("tree", sample_text1[0:12]) == ["tall"]
+    assert look_for_adjectives("hat", "The squirrel lost its tiny hat.") == \
+        ["tiny"]
     # Test find adjectives in a string with multiple other adjectives.
-    assert find_adjectives("crackers", sample_text2[12:26]) == ["crispy"]
+    assert look_for_adjectives("Miriam", "Malvina's mother's crispy crackers \
+        make a distracted Miriam very happy.") == ["distracted"]
     # Test that a word that is not a noun returns no adjectives.
-    assert find_adjectives("It", sample_text1[6:12]) == []
-    assert find_adjectives("moist", sample_text2[0:11]) == []
+    assert look_for_adjectives("It", "It was a tall tree.") == []
+    assert look_for_adjectives("moist", "Miriam bought a deliciously moist \
+        chapstick from Trader Johan's.") == []
     # Check that an adjective separated by one word from keyword is found.
-    assert find_adjectives("Miriam", sample_text2[12:27]) == ["distracted", "happy"]
-    assert find_adjectives("elbows", sample_text3[:]) == ["sharp", "hairy"]
+    assert look_for_adjectives("elbows", "She had sharp and hairy elbows.") == \
+        ["sharp", "hairy"]
 
 
 def test_expand_keywords():
@@ -76,8 +73,8 @@ def test_expand_keywords():
     of a word
     """
     # Check that a sinngle keyword is expanded.
-    assert expand_keywords(["bath"]) == ["bath", "Bath", "baths", "Baths"]
+    assert expand_keywords(["bath"]) == ["bath", "baths", "Bath", "Baths"]
     # Check that multiple keywords are expanded.
-    assert expand_keywords(["house", "frog"]) == ["house", "frog", "House",\
-         "houses", "Houses", "Frog", "frogs", "Frogs"]
-    pass
+    assert expand_keywords(["house", "frog"]) == ["house", "frog", "houses",
+                                                 "House", "Houses", "frogs",
+                                                 "Frog", "Frogs"]
